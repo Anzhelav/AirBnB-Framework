ef58366d047615bae0f27ed2c30b5d59c26c154c
Anzhelav <vasianzh@gmail.com>
2022-12-26 13:10:05 -0600

    Revert "Framework from scratch"
    This reverts commit 362423814b2bf7fc2d60b891830150aba75fcfa7.

--- src/test/java/com/automation/pages/BasePage.java
+++ src/test/java/com/automation/pages/BasePage.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
-    WebDriver driver = DriverUtils.getDriver();
-    public BasePage() {
-        PageFactory.initElements(driver, this);
-    }
+    WebDriver driver= DriverUtils.getDriver();
+    public BasePage(){
+        PageFactory.initElements(driver,this);}
+
 }
--- src/test/java/com/automation/utils/DriverUtils.java
+++ src/test/java/com/automation/utils/DriverUtils.java
@@ -5,39 +5,60 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
 
 public class DriverUtils {
-    //    public static final String USERNAME = ConfigReader.getProperty("bs.username");
-//    public static final String AUTOMATE_KEY = ConfigReader.getProperty("bs.key");
-//    public static final String URL = String.format(ConfigReader.getProperty("bs.url"),USERNAME,AUTOMATE_KEY);
-    static WebDriver driver;
 
-    public static void createDriver()  {
 
-        switch (ConfigReader.getProperty("browser")) {
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
-            default:
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+        /*public static final String USERNAME = ConfigReader.getProperty("bs.username");
+        public static final String AUTOMATE_KEY = ConfigReader.getProperty("bs.key");
+        public static final String URL = String.format(ConfigReader.getProperty("bs.url"), USERNAME, AUTOMATE_KEY);*/
+        static WebDriver driver;
+
+
+        public static void createDriver() {
+            /*DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("browserName", "Chrome");
+            capabilities.setCapability("browserVersion", "latest");
+*/
+
+
+           /* switch (ConfigReader.getProperty("browser")) {
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                case "remote":
+                    try {
+                        driver = new RemoteWebDriver(new URL(URL), capabilities);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+
+            }
+*/
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().deleteAllCookies();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+
         }
 
-        driver.manage().deleteAllCookies();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-        driver.get(ConfigReader.getProperty("app.baseurl"));
-    }
+        public static WebDriver getDriver() {
+            return driver;
+        }
 
-    public static WebDriver getDriver() {
-        return driver;
     }
-
-}
